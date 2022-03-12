0e5caa13ec32f956aa37944cd83f9a8370296593
Md-Saifullah <rssaiful67@gmail.com>
2022-03-13 02:18:51 +0600

    2nd commit added jQ code

---
+++ index.js
@@ -0,0 +1,13 @@
+function text01() {
+	document.querySelector(".style").classList.add("style1");
+}
+
+function text02() {
+	$(".styleX").css({
+		"font-size": "6rem",
+		color: "green",
+		background: "orange",
+		display: "block",
+		width: "25%",
+	});
+}
